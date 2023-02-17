b596abbb439c39ff62b0ba0286d66b9e18bc98a3
Nyx <61583597+mrlittleman@users.noreply.github.com>
2023-02-17 14:28:02 +0800

    connection added

--- core/settings.py
+++ core/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '2IlnNHFmLaEv9HKWwenB',
+        'HOST': 'containers-us-west-26.railway.app',
+        'PORT': '5820',
     }
 }
 
